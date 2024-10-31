5460ec9ddf5b21bab54a141c8bff86375626997d
Artarindo Intern <intern.it@artarindo.co.id>
2024-10-31 13:27:47 +0700

    renaming the controller

--- CareWebServiceEndpoint/obj/Debug/net8.0/CareWebServiceEndpoint.AssemblyInfo.cs
+++ CareWebServiceEndpoint/obj/Debug/net8.0/CareWebServiceEndpoint.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CareWebServiceEndpoint")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca362acf292177dc44c08aa039b909a5e2348a92")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6ee6d2af829c9d79c088424ddf31d7bd4794e51")]
 [assembly: System.Reflection.AssemblyProductAttribute("CareWebServiceEndpoint")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CareWebServiceEndpoint")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
